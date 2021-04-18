60f6c21a2a2f4fec6a8860b251bf58dbca896b1c
Flavio Titus <flavio.titus@gmail.com>
2021-04-18 18:02:59 -0300

    Initial Code

---
+++ exercicio02.js
@@ -0,0 +1,58 @@
+/*
+Exiba na tela as seguintes informações:
+Total de salários;
+Média dos salários;
+Média dos salários por cargo(Dev Full Stack Jr, Dev Full Stack Pln, Dev Full Stack Snr)
+Total de salários por cargo(Dev Full Stack Jr, Dev Full Stack Pln, Dev Full Stack Snr)
+ */
+
+colaboradores = [
+  {
+      id: 1,
+      nome: 'Thiago',
+      salario: 3000,
+      cargo: 'Dev Full Stack Jr'
+  },
+  {
+      id: 2,
+      nome: 'Matheus',
+      salario: 3200,
+      cargo: 'Dev Full Stack Jr'
+  },
+  {
+      id: 3,
+      nome: 'Otavio',
+      salario: 2600,
+      cargo: 'Dev Full Stack Jr'
+  },
+  {
+      id: 4,
+      nome: 'Jorge',
+      salario: 9000,
+      cargo: 'Dev Full Stack Snr'
+  },
+  {
+    id: 5,
+    nome: 'Alan',
+    salario: 10000,
+    cargo: 'Dev Full Stack Snr'
+},
+{
+    id: 6,
+    nome: 'David',
+    salario: 11000,
+    cargo: 'Dev Full Stack Snr'
+},
+{
+    id: 7,
+    nome: 'Gabriel',
+    salario: 7000,
+    cargo: 'Dev Full Stack Pln'
+},
+{
+    id: 8,
+    nome: 'Arthur',
+    salario: 6500,
+    cargo: 'Dev Full Stack Pln'
+},
+];
